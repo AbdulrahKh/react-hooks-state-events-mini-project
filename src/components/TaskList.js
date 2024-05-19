cc04daa4270dec23b5c737a0905dd310f0e02267
AbdulrahmanKh <abdul.rahkh19@gmail.com>
2024-05-20 01:55:20 +0300

    Done Assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, deleteTask }) {
+  const taskView = [];
+  for (let i = 0; i < tasks.length; i++) {
+    taskView.push(
+      <Task
+        key={`task${i}`}
+        text={tasks[i].text}
+        category={tasks[i].category}
+        deleteTask={() => deleteTask(i)}
+      />
+    );
+  }
+
+  return <div className="tasks">{taskView}</div>;
 }
 
 export default TaskList;
